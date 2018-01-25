16665927e1b5ce7ca16cabfb3e731fb41356de0f
bes12 <bes12@pct.edu>
2018-01-25 11:44:44 -0500

    Test

---
+++ app/src/main/java/com/example/bes12/tipcalculator/TipCalculator.java
@@ -0,0 +1,17 @@
+package com.example.bes12.tipcalculator;
+
+public class TipCalculator {
+    private float tip;
+    private float bill;
+
+    public TipCalculator(float newTip, float newBill){
+        setTip(newTip);
+        setBill(newBill);
+    }
+
+    public float getTip(){
+        return tip;
+    }
+    //getbill
+    //page 42
+}
